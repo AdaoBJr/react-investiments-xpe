85a3b7931d02eee49c64c05158ba8db69e6978e4
AdaoBJr <adaobjr@gmail.com>
2023-05-06 22:08:40 -0400

    feat: :sparkles: added title on Investiment

--- src/app/ui/Investment/Investment.tsx
+++ src/app/ui/Investment/Investment.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Box, Divider, Stack } from '@mui/material';
 
-import { Text } from 'app/shared';
+import { Text, Title } from 'app/shared';
 import { SxDividerUi } from './styles';
 import { BodyInvestiment } from './components';
+import { useInvestment } from 'services/talons';
 
 export const Investment: React.FC = () => {
+  const {
+    compProps: { title },
+  } = useInvestment();
+
   const render = [
     { month: 'jan/2020', value: 1000, revenue: '11%' },
     { month: 'fev/2020', value: 2000, revenue: '10%' },
@@ -14,6 +19,12 @@ export const Investment: React.FC = () => {
 
   return (
     <BodyInvestiment>
+      <Box>
+        <Title {...title} />
+        <Box mt={1}>
+          <Divider sx={SxDividerUi} />
+        </Box>
+      </Box>
       {render.map((item, i) => (
         <Box key={item.month}>
           <Stack flexDirection={'row'} justifyContent={'space-between'}>
