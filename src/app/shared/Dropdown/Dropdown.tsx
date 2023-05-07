2cb2b6ae151337b67097eb9c8cdbf81700309535
AdaoBJr <adaobjr@gmail.com>
2023-05-06 21:44:12 -0400

    updated dropdown

--- src/app/shared/Dropdown/Dropdown.tsx
+++ src/app/shared/Dropdown/Dropdown.tsx
@@ -4,7 +4,7 @@ import { MenuItem, TextField, Typography } from '@mui/material';
 import { SxDropdown } from './styles';
 import { DropdownProps } from 'types/shared';
 
-export const Dropdown: React.FC<DropdownProps> = ({ name, options, value, ...rest }) => (
+export const Dropdown: React.FC<DropdownProps> = ({ name, options, ...rest }) => (
   <TextField name={name} {...rest} sx={SxDropdown} select>
     {options?.map((item) => (
       <MenuItem key={item.value} value={item.value} aria-label={item?.label}>
